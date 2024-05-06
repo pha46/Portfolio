6f3a131da683721d0f0b93af41f621e594fa521b
pha46 <pravinhrne@gmail.com>
2024-05-07 01:12:44 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import abotMe from './assets/aboutme.jpg';
+import aboutMe from './assets/aboutme.jpg';
+import git from './assets/git.svg';
+import mongoDB from './assets/mongodb.svg';
+import react from './assets/react.svg';
+import nodejs from './assets/nodejs.svg';
+import express from './assets/Express.svg';
+import netlify from './assets/netlify.svg';
+import vercel from './assets/vercel.svg';
+import githubPages from './assets/github-Pages.svg';
+import mui from './assets/material-ui.svg';
+import tailwindcss from './assets/tailwind-css.svg';
+import redux from './assets/redux.svg';
+import render from './assets/render.svg';
+import { Slide } from "react-awesome-reveal";
+import Projects from './components/projects';
+import axios from 'axios';
 
 function App() {
+  const [FormData, setFormData] = useState({
+    name:"",
+    email:"",
+    message:""
+  });
+  const [responseMessage, setResponseMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (event) => {
+    setLoading(true);
+    event.preventDefault();
+    fetch('http://abcd.com/api/sendEmail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(FormData)
+    }).then(response => {
+      setLoading(false);
+      if (response.ok) {
+        setResponseMessage('Message sent successfully');
+      } else {
+        throw new Error('Failed');
+      }
+    }).catch(error => {
+      setLoading(false);
+      setResponseMessage('Error: '+ error.message);
+    });
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...FormData, [name]: value });
+  }
+  
+
   return (
     <div className="container">
       <header>
         <nav className="navbar">
           <a href="#hero">Home</a>
           <a href='#about'>About</a>
+          <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
-          <a href="#services">Services</a>
-          <a href="#testimonials">Testimonials</a>
           <a href="#contact">Contact</a>
         </nav>
+      </header>
+      <main>
         <section id="hero">
           <h1>Hi, I am Pravinkumar Harne👋</h1>
           <p>Full Stack MERN Web Developer.</p>
           <a href="#contact">Contact Me</a>
         </section>
-      </header>
-      <main>
         <section id='about'>
           <h2>About Me</h2>
           <div style={{
@@ -30,58 +80,217 @@ function App() {
             justifyContent:'space-between',
             width:'100%'
           }}>
+            <Slide direction='left'>
             <div style={{
-              backgroundImage:abotMe,
-              backgroundSize:'cover'
+              backgroundImage:`url(${aboutMe})`,
+              backgroundSize:'contain',
+              width:'1348px',
+              height:'520px'
             }}>
-            </div>
-            <div style={{
-              backgroundColor:'yellow'
+
+              <div style={{
+              backgroundColor:'transparent',
+              width:'100%',
+              height:'50%',
+              paddingTop:'5%',
+              paddingRight:'1%',
+              border:'none',
+              position:'absolute',
+              zIndex:'2'
             }}>
-              <h3>welcome</h3>
+            {/* <Slide direction='bottom'> */}
+              <div style={{
+                backgroundColor:'lavender',
+                width:'auto',
+                borderRadius:'5px',
+                marginLeft:'50%'
+              }}>
+                <p style={{
+                  textAlign:'left',
+                  padding:'10px',
+                  paddingTop:'20px',
+                  fontWeight:'lighter'
+                }}>
+                Web Developer with knowledge of React.js, Redux, node, mongoDB, HTML and CSS. <br/> currently learning React for Web Apllication.<br/><br/>
+
+I am passionate programmer and a learner, born and brought up in India. <br/> Currently, I am enrolled in AlmaBetter - Full Stack Web Development Program.<br/><br/>
+
+I love learning about new technologies, <br/> what problems are they solving and How can I use them to build better and scalable products.
+                </p>
+                <div style={{
+                  display:'flex',
+                  flexDirection:'row',
+                  justifyContent:'space-between',
+                  width:'80%',
+                  justifySelf:'center',
+                  padding:'10px',
+                  marginLeft:'10px'
+                }}>
+                  <button onClick={() => window.location.href="#contact"} style={{border:'1px solid red',color:'red'}}>Contact</button>
+                  <button style={{border:'1px solid green',color:'green'}}
+                    onClick={() => window.open('https://docs.google.com/document/d/1yeLnPVmV4DpgdSTc_cw49zpB27JaWwdR/edit?usp=sharing&ouid=100320765830359048561&rtpof=true&sd=true', '_blank')}
+                    >My Resume
+                  </button>
+                  <button style={{border:'1px solid blue',color:'blue'}}
+                    onClick={() => window.open('https://github.com/pha46/', '_blank')}  
+                    >GitHub
+                  </button>
+                  <button style={{border:'1px solid orange',color:'orange'}}
+                    onClick={() => window.open('https://www.linkedin.com/in/pravinkumar-harne')}
+                    >LinkedIn
+                  </button>
+                </div>
+              </div>
+            {/* </Slide> */}
             </div>
+            </div>
+            </Slide>
           </div>
         </section>
+        <section id="skills">
+          <h2>Skills</h2>
+          <Slide direction='right'> 
+          <div className='cards'>
+            <div className='div2 div3'>
+              <div className='div4 focus'>
+                <h3>Version Control</h3><hr/>
+                <p>
+                  <a className='card' 
+                    href="https://git-scm.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={git} alt="GIT" /> 
+                        GIT
+                  </a>
+                </p>
+              </div>
+              <div className='div4 focus'>
+                <h3>Database</h3><hr/>
+                  <a className='card' 
+                    href="https://mongodb.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={mongoDB} alt="MongoDB" /> 
+                        MongoDB
+                  </a>
+              </div>
+            </div>
+            <div className='div2 focus'>
+              <h3>Library</h3><hr/>
+                <a className='card' 
+                    href="https://react.dev/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={react} alt="React" /> 
+                        React
+                </a>
+                <a className='card' 
+                    href="https://nodejs.org/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={nodejs} alt="NodeJS" /> 
+                        NodeJS
+                  </a>
+                  <a className='card' 
+                    href="https://mui.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={mui} alt="Material UI" /> 
+                        Material UI
+                  </a>
+                  <a className='card' 
+                    href="https://redux.js.org/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={redux} alt="ReduxJS" /> 
+                        Redux JS
+                  </a>
+            </div>
+            <div className='div2 focus'>
+              <h3>Hosting Platform</h3><hr/>
+                <a className='card' 
+                    href="https://netlify.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={netlify} alt="Netlify" /> 
+                        Netlify
+                </a>
+                <a className='card' 
+                    href="https://vercel.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={vercel} alt="Vercel" /> 
+                        Vercel
+                  </a>
+                  <a className='card' 
+                    href="https://pages.github.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={githubPages} alt="Github Pages" /> 
+                        GitHub Pages
+                  </a>
+                  <a className='card' 
+                    href="https://render.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={render} alt="Render" /> 
+                        Render
+                  </a>
+            </div>
+            <div className='div2 focus'>
+              <h3>Framework</h3><hr/>
+                <a className='card' 
+                    href="https://expressjs.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={express} alt="ExpressJS" /> 
+                        Express JS
+                  </a>
+                  <a className='card' 
+                    href="https://tailwindcss.com/" 
+                    rel='noreferrer'
+                    target="_blank">
+                      <img src={tailwindcss} alt="TailwindCss" /> 
+                        Tailwind CSS
+                  </a>
+            </div>
+          </div>
+          </Slide>
+        </section><hr></hr>
         <section id="projects">
           <h2>Projects</h2>
-          <div className="project">
-            <h3>Project 1</h3>
-            <p>Description of project 1</p>
-          </div>
-          <div className="project">
-            <h3>Project 2</h3>
-            <p>Description of project 2</p>
-          </div>
-          {/* Add more project entries as needed */}
+          <Projects/>
         </section>
-        <section id="services">
-          <h2>Services</h2>
-          <ul>
-            <li>Service 1</li>
-            <li>Service 2</li>
-            <li>Service 3</li>
-            {/* Add more services as needed */}
-          </ul>
-        </section>
-        <section id="testimonials">
-          <h2>Testimonials</h2>
-          <div className="testimonial">
-            <blockquote>Client testimonial 1</blockquote>
-            <cite>- Client 1</cite>
-          </div>
-          <div className="testimonial">
-            <blockquote>Client testimonial 2</blockquote>
-            <cite>- Client 2</cite>
-          </div>
-          {/* Add more testimonials as needed */}
-        </section>
+        <hr/>
         <section id="contact">
           <h2>Contact Me</h2>
-          <form>
-            <input type="text" placeholder="Your Name" />
-            <input type="email" placeholder="Your Email" />
-            <textarea placeholder="Your Message"></textarea>
+          <form className='form' onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              placeholder="Your Name"
+              name='name'
+              value={FormData.name}
+              onChange={handleInputChange}
+              required
+            />
+            <input 
+              type="email"
+              placeholder="Your Email"
+              name='email'
+              value={FormData.email}
+              onChange={handleInputChange}
+                
+              required
+            /><br/>
+            <textarea 
+              placeholder="Your Message"
+              name='message'
+              value={FormData.message}
+              onChange={handleInputChange}
+              required
+            ></textarea><br/>
             <button type="submit">Send</button>
+            {loading===false ? responseMessage && <p>{responseMessage}</p> :<p>Please Wait...</p>}
           </form>
         </section>
       </main>
---
+++ src/components/projects.jsx
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import './projects.css';
+import { Slide } from "react-awesome-reveal";
+import resumeBuilder from '../assets/Screenshot (12).png';
+import youtubeSubscribers from '../assets/Screenshot (13).png';
+import EntertainmentApp from '../assets/Screenshot (14).png';
+
+function Projects() {
+  const [show, setShow] = useState(false);
+
+  const handleToggle = () => {
+    setShow(!show);
+  }
+
+    return (
+      <Slide direction="left">
+        <div className="projects">
+          <div className="card-container">
+              <div className="idlsoel">
+                <div className="ilvQax">
+                  <div className="project-title">
+                      <div className="project-name">
+                          <h3>
+                            Resume Builder
+                          </h3>
+                      </div>
+                  </div>
+                  <div className="marker-col">
+              <span className="marker">
+              </span>
+                  </div>
+                  <div>
+                    <div className="project-card">
+              <div>
+                <h4>Resume Builder</h4>
+                <div class="accordion">
+                  <img src={resumeBuilder}
+                alt="Resume-builder"/>
+                      <div>
+                        <button onClick={handleToggle}>
+                            PROJECT DETAILS
+                        </button>
+                        {show && 
+                        <div className="project-details">
+                          <div class="card-body">
+                      <strong>Description: </strong>
+                        This is an Resume Builder website of having different templates, which provides various user choice selectable designs.<hr/><strong>
+                        Features:</strong>
+                        <ul class="list-styles pt-1">
+                          <li>Provide selectable template Designs.</li>
+                          <li>Downloadable PDF with preview</li>
+                          <li>Minimal Designs</li></ul><hr/>
+                          <strong>Tech used:</strong>
+                        <ul>
+                          <li><span class="p-2"> React</span></li>
+                          <li><span class="p-2"> MUI Library</span></li>
+                          <li><span class="p-2"> Redux</span></li>
+                        </ul>
+                          </div>
+                        </div>}
+                      </div>
+                  </div>
+                  <div className="project-url">
+                            <a href="https://pha46.github.io/Resume-Builder/" 
+                              class="url-button" target="_blank" rel='noreferrer'>
+                                SEE LIVE
+                            </a>
+                            <a href="https://github.com/pha46/Resume-Builder.git"
+                              class="url-button" target="_blank" rel='noreferrer'>
+                                SOURCE CODE
+                            </a>
+                  </div>
+                </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <br></br>
+          <div className="card-container">
+              <div className="idlsoel">
+                <div className="ilvQax">
+                  <div className="project-title">
+                      <div className="project-name">
+                          <h3>
+                            API - Youtube Subscribers
+                          </h3>
+                      </div>
+                  </div>
+                  <div className="marker-col">
+              <span className="marker">
+              </span>
+                  </div>
+                  <div>
+                    <div className="project-card">
+                <div>
+                <h4>API - Youtube Subscribers</h4>
+                <div class="accordion">
+                  <img src={youtubeSubscribers} 
+                alt="Youtbe Subscribers"/>
+                      <div>
+                        <button onClick={handleToggle}>
+                            PROJECT DETAILS
+                        </button>
+                        {show && 
+                        <div className="project-details">
+                          <div class="card-body">
+                      <strong>Description: </strong>
+                       This is an API for getting data from a Database.<hr/><strong>
+                        Features:</strong>
+                        <ul class="list-styles pt-1">
+                          <li>Provided an Endpoint to retrieve data.</li>
+                          <li>Using ID to get data</li></ul><hr/>
+                          <strong>Tech used:</strong>
+                        <ul>
+                          <li><span class="p-2"> Node JS</span></li>
+                          <li><span class="p-2"> MongoDB</span></li>
+                          <li><span class="p-2"> Express JS</span></li>
+                        </ul>
+                          </div>
+                        </div>}
+                      </div>
+                  </div>
+                  <div className="project-url">
+                            <a href="https://youtube-subscribers-teiw.onrender.com/" 
+                              class="url-button" target="_blank" rel='noreferrer'>
+                                SEE LIVE
+                            </a>
+                            <a href="https://github.com/pha46/YouTube_Subscribers.git" 
+                              class="url-button" target="_blank" rel='noreferrer'>
+                                SOURCE CODE
+                            </a>
+                  </div>
+                </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br></br>
+            <div className="card-container">
+              <div className="idlsoel">
+                <div className="ilvQax">
+                  <div className="project-title">
+                      <div className="project-name">
+                          <h3>
+                            Entertainment APP
+                          </h3>
+                      </div>
+                  </div>
+                  <div className="marker-col">
+              <span className="marker">
+              </span>
+                  </div>
+                  <div>
+                    <div className="project-card">
+              <div>
+                <h4>Entertainment APP</h4>
+                <div class="accordion">
+                  <img src={EntertainmentApp}
+                alt="EntertainmentApp"/>
+                      <div>
+                        <button onClick={handleToggle}>
+                            PROJECT DETAILS
+                        </button>
+                        {show && 
+                        <div className="project-details">
+                          <div class="card-body">
+                      <strong>Description: </strong>
+                      This is an entertainment website that provides a searchable catalog of movies and TV shows. Users can save their favorite selections by bookmarking them for later viewing. The site also offers a portfolio builder with various template options for entertainers.<hr/><strong>
+                        Features:</strong>
+                        <ul class="list-styles pt-1">
+                          <li>Searchable database of movies and TV shows</li>
+                          <li>Bookmarking feature for saving favorite selections</li>
+                          <li>Portfolio builder with customizable templates</li></ul><hr/>
+                          <strong>Tech used:</strong>
+                        <ul>
+                          <li><span class="p-2"> Frontend: React with MUI Library</span></li>
+                          <li><span class="p-2"> Backend: Node.js with Express.js</span></li>
+                          <li><span class="p-2"> Database: MongoDB</span></li>
+                        </ul>
+                          </div>
+                        </div>}
+                      </div>
+                  </div>
+                  <div className="project-url">
+                            <a href="https://client-anhb.onrender.com/" 
+                              class="url-button" target="_blank" rel='noreferrer'>
+                                SEE LIVE
+                            </a>
+                            <a href="https://github.com/pha46/EntertainmentApp.git"
+                              class="url-button" target="_blank" rel='noreferrer'>
+                                SOURCE CODE
+                            </a>
+                  </div>
+                </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br></br>
+            <br></br>
+        </div>
+        </Slide>
+    );
+}
+
+export default Projects;
